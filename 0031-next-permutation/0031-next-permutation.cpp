16a332c5fc794ad790bbe00ff45cf73af4a02e5c
KushalKumar06 <sanjevkr7890@gmail.com>
2026-08-06 20:14:58 +0530

    Time: 0 ms (100%), Space: 15.7 MB (49.18%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,18 +4,19 @@ public:
         int pivot = -1;
         int n = nums.size();
 
-        for(int i = n-2 ; i >= 0 ; i--){
+        for(int i = n-2; i>= 0 ; i--){
             if(nums[i] < nums[i+1]){
                 pivot = i;
                 break;
             }
         }
+
         if(pivot == -1){
             reverse(nums.begin(), nums.end());
             return;
         }
 
-        for(int i = n-1; i > pivot ; i--){
+        for(int i = n-1 ; i> pivot; i--){
             if(nums[i] > nums[pivot]){
                 swap(nums[i], nums[pivot]);
                 break;
@@ -25,8 +26,9 @@ public:
         int i = pivot + 1;
         int j = n-1;
 
-        while(i<=j){
+        while(i<= j ){
             swap(nums[i], nums[j]);
+
             i++;
             j--;
         }
